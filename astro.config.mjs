c5ca206c77dc1f7d9c5228b9a8bad79c7a3cb13d
Omar Betanzos <obetanzos@it4u.com.mx>
2026-08-26 16:28:20 -0600

    OPT mobile CSS and hero image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://metalico.mx',
+  build: {
+    inlineStylesheets: 'always',
+  },
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
